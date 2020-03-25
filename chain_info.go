6037fde8e3437e2b5af56683d107bb8400221c74
mrz1836 <mrz1818@gmail.com>
2020-03-25 14:55:00 -0400

    Formatting in sections to represent documentation

--- chain_info.go
+++ chain_info.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 // GetChainInfo this endpoint retrieves various state info of the chain for the selected network.
@@ -18,5 +20,20 @@ func (c *Client) GetChainInfo() (chainInfo *ChainInfo, err error) {
 	}
 
 	err = json.Unmarshal([]byte(resp), &chainInfo)
+	return
+}
+
+// GetCirculatingSupply this endpoint retrieves the current circulating supply
+//
+// For more information: (undocumented) //todo: add link once in documentation
+func (c *Client) GetCirculatingSupply() (supply float64, err error) {
+
+	var resp string
+	// https://api.whatsonchain.com/v1/bsv/<network>/circulatingsupply
+	if resp, err = c.Request(fmt.Sprintf("%s%s/circulatingsupply", apiEndpoint, c.Parameters.Network), http.MethodGet, nil); err != nil {
+		return
+	}
+
+	supply, err = strconv.ParseFloat(strings.TrimSpace(resp), 64)
 	return
 }
--- chain_info_test.go
+++ chain_info_test.go
@@ -38,3 +38,36 @@ func ExampleClient_GetChainInfo() {
 	fmt.Println("0000000000000000057d09c9d9928c53aaff1f6b019ead3ceed52aca8abbc1c9")
 	// Output:0000000000000000057d09c9d9928c53aaff1f6b019ead3ceed52aca8abbc1c9
 }
+
+// TestClient_GetCirculatingSupply tests the GetCirculatingSupply()
+func TestClient_GetCirculatingSupply(t *testing.T) {
+	// Skip this test in short mode (not needed)
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	// Create a new client object to handle your queries (supply an API Key)
+	client, err := NewClient(NetworkMain, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var supply float64
+	if supply, err = client.GetCirculatingSupply(); err != nil {
+		t.Fatal("error occurred: " + err.Error())
+	}
+
+	if supply <= 0 {
+		t.Fatal("failed to get circulating supply")
+	}
+
+}
+
+// ExampleClient_GetCirculatingSupply example using GetCirculatingSupply()
+func ExampleClient_GetCirculatingSupply() {
+	client, _ := NewClient(NetworkMain, nil)
+	supply, _ := client.GetCirculatingSupply()
+	log.Printf("%f", supply)
+	fmt.Println("18225787.5")
+	// Output:18225787.5
+}
