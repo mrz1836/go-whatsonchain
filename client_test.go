45d1ee42955218dd1614f6279c4875fb11cbc4ca
mrz1836 <mrz1818@gmail.com>
2023-04-08 11:22:32 -0400

    Bump version

--- client_test.go
+++ client_test.go
@@ -55,7 +55,7 @@ func TestNewClient_CustomHTTPClient(t *testing.T) {
 func ExampleNewClient() {
 	client := NewClient(NetworkTest, nil, nil)
 	fmt.Println(client.UserAgent())
-	// Output:go-whatsonchain: v0.12.1
+	// Output:go-whatsonchain: v0.12.2
 }
 
 // BenchmarkNewClient benchmarks the NewClient method
