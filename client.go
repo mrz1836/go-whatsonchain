fc2faa05dfae174ed0f821562494631b04cd2bee
mrz1836 <mrz1818@gmail.com>
2023-08-14 09:36:39 -0400

    Bumped version

--- client.go
+++ client.go
@@ -12,7 +12,7 @@ import (
 const (
 
 	// version is the current version
-	version = "v0.12.2"
+	version = "v0.13.0"
 
 	// defaultUserAgent is the default user agent for all requests
 	defaultUserAgent string = "go-whatsonchain: " + version
--- client_test.go
+++ client_test.go
@@ -55,7 +55,7 @@ func TestNewClient_CustomHTTPClient(t *testing.T) {
 func ExampleNewClient() {
 	client := NewClient(NetworkTest, nil, nil)
 	fmt.Println(client.UserAgent())
-	// Output:go-whatsonchain: v0.12.2
+	// Output:go-whatsonchain: v0.13.0
 }
 
 // BenchmarkNewClient benchmarks the NewClient method
